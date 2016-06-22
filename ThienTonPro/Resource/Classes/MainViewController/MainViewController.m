b62629bb2e1eb590a7e6b5cba1e31f2165873ccb
tiepvv <tiepvv@newwave.vn>
2016-06-22 14:50:50 +0700

    update

--- ThienTonPro/Resource/Classes/MainViewController/MainViewController.m
+++ ThienTonPro/Resource/Classes/MainViewController/MainViewController.m
@@ -11,6 +11,13 @@
 
 @interface MainViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *btnAvatar;
+@property (weak, nonatomic) IBOutlet UILabel *lb_namesong;
+@property (weak, nonatomic) IBOutlet UILabel *lb_singer;
+@property (weak, nonatomic) IBOutlet UIButton *btn_Play_and_Stop;
+
+@property (nonatomic) BOOL isState;
+
 @end
 
 @implementation MainViewController
@@ -18,6 +25,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.btnAvatar.layer.cornerRadius = self.btnAvatar.frame.size.height/2;
+    self.btnAvatar.layer.masksToBounds = YES;
+    self.isState = false;
+    self.btn_Play_and_Stop.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"play.png"]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,6 +37,28 @@
 }
 - (IBAction)btnLeftSideAction:(id)sender {
     [self.menuContainerViewController toggleLeftSideMenuCompletion:nil];
+}
+
+- (IBAction)btnAvatarAction:(id)sender {
+    
+}
+
+//handle play song
+- (IBAction)btn_Play_and_Stop:(id)sender {
+    if (self.isState) {
+        self.btn_Play_and_Stop.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"play.png"]];
+        self.isState = false;
+    } else {
+        self.btn_Play_and_Stop.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"stop.png"]];
+        self.isState = true;
+    }
+}
+
+- (IBAction)btn_Next:(id)sender {
+    
+}
+- (IBAction)btn_Preview:(id)sender {
+    
 }
 
 @end
